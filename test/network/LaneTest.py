90d92b5c42e8b7eed16ea0f760e945262cc1d7b6
wanderanimrod <wanderanimrod@gmail.com>
2014-04-15 23:34:30 +0300

    Refactoring tests. Only instantiating objects we care about when setting up for testing. Should be a pattern that permeates the app

--- test/network/LaneTest.py
+++ test/network/LaneTest.py
@@ -7,20 +7,22 @@ from models.network.TwoLaneOneWayEdge import TwoLaneOneWayEdge
 class LaneTest(TestCase):
 
     def setUp(self):
-        self.edge = TwoLaneOneWayEdge(0)
-        self.lane = Lane(0, self.edge)
+        self.lane = self.make_lane()
 
-    # def test_should_add_itself_to_lane_upon_instantiation(self):
+    # def test_should_add_itself_to_edge_upon_instantiation(self):
     #     add_lane_stub = Mock(TwoLaneOneWayEdge.add_lane)
         # TODO Check that add_lane is called on parent edge
         # add_lane_stub.assert_called_once_with(self.lane)
 
     def test_should_equate_lanes_by_id(self):
-        edge = TwoLaneOneWayEdge(0)
-        lane_1 = Lane(0, edge)
-        lane_2 = Lane(0, edge)
+        lane_1 = self.make_lane(id=1)
+        lane_2 = self.make_lane(id=1)
         self.assertEqual(lane_1, lane_2)
 
     def test_should_add_vehicle_to_itself(self):
         vehicle = Vehicle(0, self.lane)
         self.assertTrue(self.lane.vehicles.__contains__(vehicle))
+
+    def make_lane(self, id=0):
+        edge = TwoLaneOneWayEdge(0)
+        return Lane(id, edge)
