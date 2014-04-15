c8172af9ff4f35f69cc633b9c2394a9a5baf9b59
wanderanimrod <wanderanimrod@gmail.com>
2014-04-15 23:13:55 +0300

    Added Vehicle class. With some not to be forgotten test case reminders

---
+++ src/models/agents/Vehicle.py
@@ -0,0 +1,6 @@
+class Vehicle:
+
+    def __init__(self, vehicle_id, lane):
+        self.id = vehicle_id
+        self.lane = lane
+        pass
---
+++ test/agents/VehicleTest.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class VehicleTest(TestCase):
+
+    def test_should_add_itself_to_parent_lane_upon_instantiation(self):
+        # TODO Implement this once you have figured out the mock stuff
+        pass
