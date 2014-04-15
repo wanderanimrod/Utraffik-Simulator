fbad1d9d03ff347805460a5b62dbf9974c60c2a1
wanderanimrod <wanderanimrod@gmail.com>
2014-04-15 22:33:23 +0300

    Lanes are now equated by ID

--- src/models/network/Lane.py
+++ src/models/network/Lane.py
@@ -3,3 +3,6 @@ class Lane:
         self.id = lane_id
         self.parent_edge = edge
         self.parent_edge.add_lane(self)
+
+    def __eq__(self, other):
+        return self.id == other.id
--- test/network/LaneTest.py
+++ test/network/LaneTest.py
@@ -6,8 +6,16 @@ from mock import Mock
 
 class LaneTest(TestCase):
 
+    def setUp(self):
+        self.edge = TwoLaneOneWayEdge(0)
+
     def test_should_add_itself_to_lane_upon_instantiation(self):
-        edge = TwoLaneOneWayEdge(0)
         add_lane_stub = Mock(TwoLaneOneWayEdge.add_lane)
-        lane = Lane(0, edge)
+        lane = Lane(0, self.edge)
+        # TODO Check that add_lane is called on parent edge
         add_lane_stub.assert_called_once_with(lane)
+
+    def test_should_equate_lanes_by_id(self):
+        lane_1 = Lane(0, self.edge)
+        lane_2 = Lane(0, self.edge)
+        self.assertEqual(lane_1, lane_2)
