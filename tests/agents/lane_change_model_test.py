42e261686c2b049d1939b3576f5a4b693d08408e
wanderanimrod <wanderanimrod@gmail.com>
2014-04-19 21:59:42 +0300

    refactoring for more readable lane change tests

--- tests/agents/lane_change_model_test.py
+++ tests/agents/lane_change_model_test.py
@@ -13,32 +13,37 @@ class LaneChangeModelTest(TestCase):
         self.target_lane = mock()
         when(self.lane).get_next_lane().thenReturn(self.target_lane)
         self.vehicle = Vehicle(0, self.lane)
-        self.follower_nearby = VehicleFactory.make_dummy_leader()
         self.make_should_change_lane_method_visible(self.vehicle)
 
     def test_should_okay_lane_change_if_acceleration_is_less_than_max_acceleration(self):
         vehicle = self.make_lane_change_scenario_with_ample_clearance(self.vehicle)
         vehicle.acceleration = vehicle.max_acceleration / 2
-        should_change_lane = vehicle.should_change_lane()
-        self.assertTrue(should_change_lane)
+        self.assertTrue(vehicle.should_change_lane())
 
     def test_should_not_change_lane_if_acceleration_is_equal_to_max_acceleration(self):
-        self.vehicle.acceleration = self.vehicle.max_acceleration
-        should_change_lane = self.vehicle.should_change_lane()
-        self.assertFalse(should_change_lane)
+        vehicle = self.make_lane_change_scenario_with_ample_clearance(self.vehicle)
+        vehicle.acceleration = vehicle.max_acceleration
+        self.assertFalse(vehicle.should_change_lane())
 
     def test_should_not_okay_lane_change_if_clearance_from_prospective_follower_will_not_be_enough(self):
-        self.follower_nearby.position = self.vehicle.position - self.vehicle.length + (Vehicle.min_clearance / 2)
-        when(self.target_lane).get_prospective_follower(self.vehicle).thenReturn(self.follower_nearby)
-        should_change_lane = self.vehicle.should_change_lane()
-        self.assertFalse(should_change_lane)
+        vehicle = self.make_lane_change_scenario_with_little_clearance(self.vehicle)
+        self.assertFalse(vehicle.should_change_lane())
 
     def make_lane_change_scenario_with_ample_clearance(self, leader):
         leader.position = 100
         follower_far_away = VehicleFactory.make_dummy_leader()
         follower_far_away.position = 0
-        when(self.target_lane).get_prospective_follower(leader).thenReturn(follower_far_away)
+        self.mock_prospective_follower(leader, follower_far_away)
         return leader
+
+    def make_lane_change_scenario_with_little_clearance(self, leader):
+        follower_nearby = VehicleFactory.make_dummy_follower()
+        follower_nearby.position = leader.position - leader.length + (Vehicle.min_clearance / 2)
+        self.mock_prospective_follower(leader, follower_nearby)
+        return leader
+
+    def mock_prospective_follower(self, leader, follower):
+        when(self.target_lane).get_prospective_follower(leader).thenReturn(follower)
 
     def make_should_change_lane_method_visible(self, vehicle):
         vehicle.should_change_lane = vehicle._Vehicle__should_change_lane
