d92ce5b94a970f5398b328f1a90c186757b03dc1
wanderanimrod <wanderanimrod@gmail.com>
2014-04-19 13:44:09 +0300

    Falied to fix mocking

--- tests/agents/vehicle_test.py
+++ tests/agents/vehicle_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from mockito import mock
+from mockito import mock, verify
 
 from models.agents.vehicle import Vehicle
 
@@ -8,7 +8,10 @@ from models.agents.vehicle import Vehicle
 class VehicleTest(TestCase):
 
     def test_should_add_itself_to_parent_lane_upon_instantiation(self):
-        # TODO Implement this once you have figured out the mock stuff
+        # TODO Fix this mocking stuff. Mockito is fake.
+        # my_mock = mock()
+        # Vehicle(0, my_mock)
+        # verify(my_mock).add_vehicle()
         pass
 
     def test_should_be_created_with_position_zero(self):
