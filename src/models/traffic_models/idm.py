9683533f3da867fddb90e3786c0037ed357e45b3
wanderanimrod <wanderanimrod@gmail.com>
2014-04-20 00:35:45 +0300

    Refactoring IDM to better naming

--- src/models/traffic_models/idm.py
+++ src/models/traffic_models/idm.py
@@ -10,12 +10,12 @@ class Idm:
 
     # Variable names used as described in Wikipedia entry on IDM
     @classmethod
-    def calculate_acceleration(cls, follower, leader):
-        if not follower.desired_velocity: return 0.0
-        v = follower.velocity
-        a = follower.max_acceleration
-        b = follower.desired_deceleration
+    def calculate_acceleration(cls, requester, leader):
+        if not requester.desired_velocity: return 0.0
+        v = requester.velocity
+        a = requester.max_acceleration
+        b = requester.desired_deceleration
         delta_v = v - leader.velocity
-        s = leader.position - follower.position - leader.length
+        s = leader.position - requester.position - leader.length
         s_star = Vehicle.min_clearance + (v * cls.T) + ((v * delta_v) / (2 * (a * b) ** 0.5))
-        return a * (1 - (v / follower.desired_velocity) ** cls.delta - (s_star / s) ** 2)
+        return a * (1 - (v / requester.desired_velocity) ** cls.delta - (s_star / s) ** 2)
