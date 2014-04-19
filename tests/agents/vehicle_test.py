dd3b6fe4211447866b65396fab7a461665f4e1f6
wanderanimrod <wanderanimrod@gmail.com>
2014-04-19 13:48:03 +0300

    Optimising

--- tests/agents/vehicle_test.py
+++ tests/agents/vehicle_test.py
@@ -1,18 +1,15 @@
 from unittest import TestCase
 
-from mockito import mock, verify
+from mockito import mock
 
 from models.agents.vehicle import Vehicle
 
 
 class VehicleTest(TestCase):
 
-    def test_should_add_itself_to_parent_lane_upon_instantiation(self):
+    # def test_should_add_itself_to_parent_lane_upon_instantiation(self):
         # TODO Fix this mocking stuff. Mockito is fake.
-        # my_mock = mock()
-        # Vehicle(0, my_mock)
-        # verify(my_mock).add_vehicle()
-        pass
+        # pass
 
     def test_should_be_created_with_position_zero(self):
         vehicle = Vehicle(0, mock())
