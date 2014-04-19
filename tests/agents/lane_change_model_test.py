d1cd80100e2f2513bb29d3af83d1749027425699
wanderanimrod <wanderanimrod@gmail.com>
2014-04-19 21:44:27 +0300

    Refactoring lane change tests to be more readable

--- tests/agents/lane_change_model_test.py
+++ tests/agents/lane_change_model_test.py
@@ -14,15 +14,12 @@ class LaneChangeModelTest(TestCase):
         when(self.lane).get_next_lane().thenReturn(self.target_lane)
         self.vehicle = Vehicle(0, self.lane)
         self.follower_nearby = VehicleFactory.make_dummy_leader()
-        self.follower_far_away = VehicleFactory.make_dummy_leader()
-        self.follower_far_away.position = 0
         self.make_should_change_lane_method_visible(self.vehicle)
-    
+
     def test_should_okay_lane_change_if_acceleration_is_less_than_max_acceleration(self):
-        self.vehicle.position = 100
-        when(self.target_lane).get_prospective_follower(self.vehicle).thenReturn(self.follower_far_away)
-        self.vehicle.acceleration = self.vehicle.max_acceleration / 2
-        should_change_lane = self.vehicle.should_change_lane()
+        vehicle = self.make_lane_change_scenario_with_ample_clearance(self.vehicle)
+        vehicle.acceleration = vehicle.max_acceleration / 2
+        should_change_lane = vehicle.should_change_lane()
         self.assertTrue(should_change_lane)
 
     def test_should_not_change_lane_if_acceleration_is_equal_to_max_acceleration(self):
@@ -35,6 +32,13 @@ class LaneChangeModelTest(TestCase):
         when(self.target_lane).get_prospective_follower(self.vehicle).thenReturn(self.follower_nearby)
         should_change_lane = self.vehicle.should_change_lane()
         self.assertFalse(should_change_lane)
+
+    def make_lane_change_scenario_with_ample_clearance(self, leader):
+        leader.position = 100
+        follower_far_away = VehicleFactory.make_dummy_leader()
+        follower_far_away.position = 0
+        when(self.target_lane).get_prospective_follower(leader).thenReturn(follower_far_away)
+        return leader
 
     def make_should_change_lane_method_visible(self, vehicle):
         vehicle.should_change_lane = vehicle._Vehicle__should_change_lane
