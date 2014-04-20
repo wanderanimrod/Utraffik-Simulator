0caec84b1f89a37e3039c298523f2a1f17901f8f
wanderanimrod <wanderanimrod@gmail.com>
2014-04-20 11:04:45 +0300

    there is no need to have the VehicleFactory static methods as class methods since they don't refer to themselves

--- src/models/agents/vehicle_factory.py
+++ src/models/agents/vehicle_factory.py
@@ -7,15 +7,15 @@ class VehicleFactory:
     def __init__(self):
         pass
 
-    @classmethod
-    def make_dummy_leader(cls):
+    @staticmethod
+    def make_dummy_leader():
         leader = Vehicle(-1, mock())
         leader.velocity = 33.3
         leader.position = 100000
         return leader
 
-    @classmethod
-    def make_dummy_follower(cls):
+    @staticmethod
+    def make_dummy_follower():
         follower = Vehicle(-2, mock())
         follower.desired_velocity = 0
         return follower
