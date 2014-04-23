8fa73c13220f09f8e194d4a8343c7408b70c9a8b
wanderanimrod <wanderanimrod@gmail.com>
2014-04-23 23:52:49 +0300

    getting rid of Java class mentality

--- src/models/agents/vehicle_factory.py
+++ src/models/agents/vehicle_factory.py
@@ -2,24 +2,18 @@ from mockito import mock
 from models.agents.vehicle import Vehicle
 
 
-class VehicleFactory:
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def make_dummy_leader():
-        leader = Vehicle(-1, mock())
-        leader.velocity = 33.3
-        leader.position = 100000
-        return leader
-
-    @staticmethod
-    def make_dummy_follower():
-        follower = Vehicle(-2, mock())
-        follower.desired_velocity = 0
-        return follower
+def make_dummy_leader():
+    leader = Vehicle(-1, mock())
+    leader.velocity = 33.3
+    leader.position = 100000
+    return leader
 
 
-dummy_leader = VehicleFactory.make_dummy_leader()
-dummy_follower = VehicleFactory.make_dummy_follower()
+def make_dummy_follower():
+    follower = Vehicle(-2, mock())
+    follower.desired_velocity = 0
+    return follower
+
+
+dummy_leader = make_dummy_leader()
+dummy_follower = make_dummy_follower()
