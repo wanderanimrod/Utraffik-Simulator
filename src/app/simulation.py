83199054461c88ea6ad284ecb4ca4b7e37714d57
wanderanimrod <wanderanimrod@gmail.com>
2014-05-15 20:04:29 +0300

    At the point at which we have to figure our sim clock issues

--- src/app/simulation.py
+++ src/app/simulation.py
@@ -1,18 +1,23 @@
 from multiprocessing import Queue, Process
+
+from app.translator import Translator
 from models.agents.vehicle import Vehicle
 from models.network.lane import Lane
 from models.network.two_lane_one_way_edge import TwoLaneOneWayEdge
 
+
 vehicles_queue_1 = Queue()
-process_2_vehicle_queue = Queue()
 
 
-def start_translator(network, vehicles_queue):
-    pass
+def start_translator(sub_network, vehicles_queue):
+    translator = Translator(sub_network)
+    while not translator.is_waiting:
+        translator.sweep(10)  # TODO Figure out sim timing
 
 
 def run():
     sub_net_1 = load_network()
+    start_sim_clock()
     process = Process(target=start_translator, args=(sub_net_1, vehicles_queue_1))
     process.start()
     process.join()
@@ -26,3 +31,7 @@ def load_network():
     Vehicle(0, lane_1)
     Vehicle(0, lane_2)
     return [edge]
+
+
+def start_sim_clock():
+    pass
