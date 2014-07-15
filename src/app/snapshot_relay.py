ee70596e6e799139766c7bcafac2efe85bcd665b
wanderanimrod <wanderanimrod@gmail.com>
2014-07-15 23:11:16 +0300

    Working, tested version of snapshot relay. Puts snapshots on a queue passed when relay object is being instantiated

--- src/app/snapshot_relay.py
+++ src/app/snapshot_relay.py
@@ -3,24 +3,10 @@ from Queue import Queue
 from app.events.events import E_TRANSLATE
 
 
-vehicle_snapshots_queue = Queue()
-
-
 class SnapshotRelay:
-    def __init__(self):
+    def __init__(self, snapshots):
+        self.__snapshots = snapshots
         E_TRANSLATE.connect(self.__add_snapshot)
 
-    @staticmethod
-    def __add_snapshot(sender, **kwargs):
-        vehicle_snapshots_queue.put(sender)
-        print "Snapshot added for vehicle : ", sender
-
-
-class ReferenceRelay:
-
-    def __init__(self, q):
-        self.q = q
-        E_TRANSLATE.connect(self.add)
-
-    def add(self, sender, **kwargs):
-        self.q.put(sender)
+    def __add_snapshot(self, sender, **kwargs):
+        self.__snapshots.put(sender)
--- tests/app_tests/snapshot_relay_test.py
+++ tests/app_tests/snapshot_relay_test.py
@@ -1,22 +1,32 @@
 from Queue import Queue
 from unittest import TestCase
+
 from mockito import mock
+
 from app.events.events import E_TRANSLATE
-from app.snapshot_relay import SnapshotRelay, vehicle_snapshots_queue, ReferenceRelay
+from app.snapshot_relay import SnapshotRelay
+
 from models.agents.vehicle import Vehicle
 
 
 class SnapshotRelayTest(TestCase):
 
-    def test_should_put_vehicle_snapshot_on_the_sim_queue_when_translation_event_is_fired(self):
-        SnapshotRelay()
-        vehicle = Vehicle(0, mock())
-        E_TRANSLATE.send(sender=vehicle)
-        self.assertEqual(vehicle_snapshots_queue.get_nowait(), vehicle)
+    """
+        A note about this line: relay = SnapshotRelay(snapshots)
+        The SnapshotRelay object will not put anything on its internal queue if when it is instantiated,
+         the instance is not assigned to an variable.
 
-    def test_reference_relay(self):
-        q = Queue()
-        relay = ReferenceRelay(q)
+         Also, in the case of the tests, it will not work with an instance of multiprocessing.Queue.
+         It only works with Queue.Queue. This should be the first point of examination if snapshot relaying
+         doesn't work in the multiprocess environment the production code is meant to run in.
+
+         This is very strange. WHAT DOES PYTHON DO WHEN YOU ASSIGN A NEW INSTANCE OF A CLASS TO A VARIABLE
+         THAT IT DOESN'T DO WHEN A VARIABLE IS NOT CREATED FOR THE INSTANCE??
+
+        """
+    def test_should_put_vehicle_snapshot_on_the_sim_queue_when_translation_event_is_fired(self):
+        snapshots = Queue()
+        relay = SnapshotRelay(snapshots)  # See notes about this line at the start of the method
         vehicle = Vehicle(0, mock())
         E_TRANSLATE.send(sender=vehicle)
-        self.assertEqual(q.get_nowait(), vehicle)
+        self.assertEqual(snapshots.get_nowait(), vehicle)
