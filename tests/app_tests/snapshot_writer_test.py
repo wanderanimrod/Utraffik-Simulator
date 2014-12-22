de7c54a36bc678884a234c3a2ca3444df667866e
wanderanimrod <wanderanimrod@gmail.com>
2014-12-22 23:29:04 +0300

    Snapshot writer test updated to match the id+time key type of snapshots in prod code

--- tests/app_tests/snapshot_writer_test.py
+++ tests/app_tests/snapshot_writer_test.py
@@ -3,6 +3,12 @@ from unittest import TestCase
 
 from redis import StrictRedis
 
+import settings
+
+
+PROD_SNAPSHOTS_DB = settings.SNAPSHOTS_DB
+settings.SNAPSHOTS_DB = 10
+
 from app.snapshot_writer import SnapshotWriter
 from settings import SNAPSHOTS_DB, REDIS
 
@@ -13,8 +19,8 @@ class SnapshotConsumerTest(TestCase):
         self.db = StrictRedis(host=REDIS['host'], port=REDIS['port'], db=SNAPSHOTS_DB)
 
     def test_should_read_snapshots_from_queue_periodically_and_write_them_to_redis_db(self):
-        snapshot_one = {'id': 1, 'other_details': 'Vehicle data'}
-        snapshot_two = {'id': 2, 'other_details': 'Vehicle data'}
+        snapshot_one = {'id': 1, 'other_details': 'Vehicle data', 'time': 1.0}
+        snapshot_two = {'id': 2, 'other_details': 'Vehicle data', 'time': 1.0}
         [self.snapshot_queue.put(item) for item in [snapshot_one, snapshot_two]]
 
         writer = SnapshotWriter(self.snapshot_queue)
@@ -23,8 +29,8 @@ class SnapshotConsumerTest(TestCase):
         writer.wait_for_work_to_end(timeout=0.3)
         writer.shutdown()
 
-        snapshots_in_db = [self.db.hgetall(key) for key in ['snapshot_1', 'snapshot_2']]
-        expected_snapshots = [self.stringify_id_for(snapshot) for snapshot in [snapshot_one, snapshot_two]]
+        snapshots_in_db = [self.db.hgetall(key) for key in ['snapshot_1:1.000000', 'snapshot_2:1.000000']]
+        expected_snapshots = [self.redisify_snapshot(snapshot) for snapshot in [snapshot_one, snapshot_two]]
 
         self.assertListEqual(snapshots_in_db, expected_snapshots)
 
@@ -34,11 +40,13 @@ class SnapshotConsumerTest(TestCase):
         writer.shutdown()
         self.assertTrue(True)  # Tests that we get here
 
-    def stringify_id_for(self, snapshot):
-        snapshot.update({'id': str(snapshot['id'])})
+    def redisify_snapshot(self, snapshot):
+        snapshot.update({'id': str(snapshot['id']), 'time': "%.1f" % snapshot['time']})
         return snapshot
 
     def tearDown(self):
-        self.db.delete('snapshot_1', 'snapshot_2')
+        for key in self.db.keys("*"):
+            self.db.delete(key)
 
 
+settings.SNAPSHOTS_DB = PROD_SNAPSHOTS_DB
